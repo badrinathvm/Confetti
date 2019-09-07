b2d73177d65d7c5e696d729d062fff3ccebc9b30
badrinathvm@gmail.com <badrinathvm@gmail.com>
2019-09-07 13:07:37 -0700

    Added key animation frame to auto fadeout after some time

--- Confetti/Confetti/View/ConfettiView.swift
+++ Confetti/Confetti/View/ConfettiView.swift
@@ -18,6 +18,7 @@ enum ConfettiType {
     case image(UIImage)
 }
 
+private let kAnimationLayerKey = "com.confetti.animationLayer"
 
 class ConfettiView : UIView {
     
@@ -47,12 +48,43 @@ class ConfettiView : UIView {
         intensity = 0.5
     }
     
+    ///creates a keyframe animation to taper the birthRate property down to 0 over the specified duration.
+    fileprivate func keyFrameAnimation() {
+        let animation = CAKeyframeAnimation(keyPath: #keyPath(CAEmitterLayer.birthRate))
+        animation.duration = 10.0
+        animation.timingFunction = CAMediaTimingFunction(name: .easeIn)
+        animation.fillMode = .backwards
+        animation.values = [1, 0, 0]
+        animation.keyTimes = [0, 0.5, 1]
+        animation.isRemovedOnCompletion = false
+        
+        layer.beginTime = CACurrentMediaTime()
+        emitter.birthRate = 1.0
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            let transition = CATransition()
+            transition.delegate = self
+            transition.type = .push
+            transition.duration = 1
+            transition.timingFunction = CAMediaTimingFunction(name: .easeOut)
+            transition.setValue(self.emitter, forKey: kAnimationLayerKey)
+            transition.isRemovedOnCompletion = false
+            
+            self.emitter.add(transition, forKey: nil)
+            
+            self.emitter.opacity = 0
+        }
+        layer.add(animation, forKey: nil)
+        CATransaction.commit()
+    }
+    
     func startConfetti() {
         //A layer that emits, animates, and renders a particle system.
         emitter = CAEmitterLayer()
         
         emitter.emitterPosition = CGPoint(x: frame.size.width / 2.0, y: 0)
-        emitter.emitterShape = CAEmitterLayerEmitterShape.sphere
+        emitter.emitterShape = CAEmitterLayerEmitterShape.line
         emitter.emitterSize = CGSize(width: frame.size.width, height: 1)
         
         //construct the cells
@@ -60,6 +92,8 @@ class ConfettiView : UIView {
         for color in colors {
             cells.append(confettiWithColor(color: color))
         }
+        
+        keyFrameAnimation()
         
         emitter.emitterCells = cells
         layer.addSublayer(emitter)
@@ -72,32 +106,43 @@ class ConfettiView : UIView {
     func confettiWithColor(color: UIColor) -> CAEmitterCell {
         let confetti = CAEmitterCell()
         
-        //The number of emitted objects created every second. Animatable.
-        confetti.birthRate = 6.0 * intensity
-        
-        //The lifetime of the cell, in seconds.
-        confetti.lifetime = 14.0 * intensity
-        confetti.lifetimeRange = 0.0
-        
+//        //The number of particles created every second.
+        //confetti.birthRate = 2.0
+//
+//        //The lifetime of the cell, in seconds.
+//        confetti.lifetime = 14.0 * intensity
+//        confetti.lifetimeRange = 0.0
+//
+//        confetti.color = color.cgColor
+//
+        //confetti.velocity = CGFloat(350.0 * intensity)
+        //confetti.velocityRange = CGFloat(80.0 * intensity)
+
+//        //The longitudinal orientation of the emission angle
+        //confetti.emissionLongitude = CGFloat(Double.pi)
+        //confetti.emissionRange = CGFloat(Double.pi)
+//
+//        //The rotational velocity, measured in radians per second, to apply to the cell.
+        //confetti.spin = CGFloat(3.5 * intensity)
+        //confetti.spinRange = CGFloat(4.0 * intensity)
+//
+//        //Specifies the range over which the scale value can vary
+        //confetti.scaleRange = CGFloat(intensity)
+        //confetti.scaleSpeed = CGFloat(-0.1 * intensity)
+//
+    
+        confetti.birthRate = 50.0
         confetti.color = color.cgColor
-        
-        confetti.velocity = CGFloat(350.0 * intensity)
-        confetti.velocityRange = CGFloat(80.0 * intensity)
-        
-        //The longitudinal orientation of the emission angle
-        confetti.emissionLongitude = CGFloat(Double.pi)
-        confetti.emissionRange = CGFloat(Double.pi)
-        
-        //The rotational velocity, measured in radians per second, to apply to the cell.
-        confetti.spin = CGFloat(3.5 * intensity)
-        confetti.spinRange = CGFloat(4.0 * intensity)
-        
-        //Specifies the range over which the scale value can vary
-        confetti.scaleRange = CGFloat(intensity)
-        confetti.scaleSpeed = CGFloat(-0.1 * intensity)
-        
+        confetti.lifetime = 10.0
+        confetti.velocity = CGFloat(confetti.birthRate * confetti.lifetime)
+        confetti.velocityRange = confetti.velocity / 2
+        confetti.emissionLongitude = .pi
+        confetti.emissionRange = .pi / 4
+        confetti.spinRange = .pi * 8
+        confetti.scaleRange = 0.25
+        confetti.scale = 1.0 - confetti.scaleRange
         confetti.contents = imageForType(type: type)?.cgImage
-        
+ 
         return confetti
     }
     
@@ -127,5 +172,15 @@ class ConfettiView : UIView {
             print(error)
         }
         return nil
+    }
+}
+
+// MARK: - CAAnimationDelegate
+extension ConfettiView: CAAnimationDelegate {
+    public func animationDidStop(_ animation: CAAnimation, finished flag: Bool) {
+        if let layer = animation.value(forKey: kAnimationLayerKey) as? CALayer {
+            layer.removeAllAnimations()
+            layer.removeFromSuperlayer()
+        }
     }
 }
--- Confetti/Confetti/ViewController.swift
+++ Confetti/Confetti/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let confettiView = ConfettiView(frame: self.view.bounds)
+        let confettiView = ConfettiView(frame: CGRect(x: 0, y: 150, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
         
         // Set colors (default colors are red, green and blue)
         confettiView.colors = [UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
@@ -22,24 +22,39 @@ class ViewController: UIViewController {
                                UIColor(red:0.48, green:0.78, blue:0.64, alpha:1.0),
                                UIColor(red:0.30, green:0.76, blue:0.85, alpha:1.0),
                                UIColor(red:0.58, green:0.39, blue:0.55, alpha:1.0)]
-        
-        
+
+
         // Set intensity (from 0 - 1, default intensity is 0.5)
         confettiView.intensity = 0.7
-        
+
         confettiView.type = .diamond
         //confettiView.type = ConfettiType.image(UIImage(named: "smiley")!)
-        
+
         // change to desired number of seconds (in this case 5 seconds)
-        let when = DispatchTime.now() + 5
+        let when = DispatchTime.now() + 10
         DispatchQueue.main.asyncAfter(deadline: when){
-            confettiView.stopConfetti()
+            //confettiView.stopConfetti()
         }
-        
+
         self.view.addSubview(confettiView)
-        
+
         confettiView.startConfetti()
+        
+       
+        
+        let confetti = HipsterConfettiView()
+         //self.view.addSubview(confetti)
+        let star = UIImage(named: "star")!
+        confetti.emit(with: [
+            //.text("👨🏻"),
+            //.text("📱"),
+            .shape(.triangle, .purple),
+            //.shape(.triangle, .lightGray),
+            //.image(star, .orange)
+        ])
     }
+    
+
     
 }
 
